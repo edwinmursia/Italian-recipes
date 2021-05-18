520f7818b9dc014a2a8dd0c278e961ca213a27e3
edwinmursia <edwin.mursia@edu.turkuamk.fi>
2021-05-18 19:47:03 +0300

    Added a function to the program.

--- theGUI.py
+++ theGUI.py
@@ -11,30 +11,55 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 cards = soup.find_all('article', 'gz-card gz-card-horizontal gz-mBottom3x gz-horizontal-view gz-ets-serp-target')
-print('Amount of recipes:', len(cards))
 
 # Prototyping extraction of a single meal.
 card = cards[0]
 
-# Title of the meal:
-search_title = card.h2.a
-title = search_title.get('title')
-print(title)   # This will be printed in the GUI.
-recipe_url = 'https://www.giallozafferano.com/' + search_title.get('href')
+# Generalizing the model with a function.
+def get_recipe(card):
+    
+    # Title of the meal:
+    search_title = card.h2.a
+    title = search_title.get('title')
+    recipe_url = 'https://www.giallozafferano.com/' + search_title.get('href')
+ 
+    # Description of the meal:
+    description = card.find('div', 'gz-description').text
 
-# Description of the meal:
-description = card.find('div', 'gz-description').text
-print(description)   # This will be printed in the GUI.
+    # Course of the meal:
+    course = card.find('div', 'gz-category').text.strip()
 
-# Course of the meal:
-course = card.find('div', 'gz-category').text.strip()
-print(course)   # This will be printed in the GUI.
+    # Difficulty of the meal:
+    difficulty = card.find('li', 'gz-single-data-recipe').text.strip()
 
-# Difficulty of the meal:
-difficulty = card.find('li', 'gz-single-data-recipe').text.strip()
-print(difficulty)   # This will be printed in the GUI.
+    # Image url of the meal:
+    img = card.picture.img
+    img_src = img.get('data-src')
 
-# Image url of the meal:
-img = card.picture.img
-img_src = img.get('data-src')
-print(img_src)    # Print will be taken out.
+    recipe = (title, description, course, difficulty, img_src, recipe_url)
+
+    return recipe
+
+recipes = []
+
+for card in cards:
+    recipe = get_recipe(card)
+    recipes.append(recipe)
+
+print(recipes) # How many recipes to print?
+
+while True:
+    try:
+        url = 'https://www.giallozafferano.com/' + soup.find('a', {'title' : 'Next page'}).get('href')
+    except AttributeError:
+        break
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    cards = soup.find_all('article', 'gz-card gz-card-horizontal gz-mBottom3x gz-horizontal-view gz-ets-serp-target')
+
+    for card in cards:
+        recipe = get_recipe(card)
+        recipes.append(recipe)
+
+print('Amount of recipes:', len(recipes))
