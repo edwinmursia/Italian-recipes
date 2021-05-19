b5eb72c69f93254d38d997091cb9c9ad8e675bff
edwinmursia <edwin.mursia@edu.turkuamk.fi>
2021-05-19 21:42:36 +0300

    Created the tkinter GUI

--- theGUI.py
+++ theGUI.py
@@ -1,9 +1,10 @@
-import webbrowser
+'''import webbrowser
 import requests
 import sys
-from bs4 import *
+from bs4 import *'''
+import tkinter as tk
 
-# Search for the specific ingredient. 
+'''# Search for the specific ingredient. 
 yourSearch = input(str('Type an ingredient: '))
 url = 'https://www.giallozafferano.com/recipes-search/' + yourSearch
 webbrowser.open_new(url)   # This will be printed in the GUI.
@@ -62,4 +63,34 @@ while True:
         recipe = get_recipe(card)
         recipes.append(recipe)
 
-print('Amount of recipes:', len(recipes))
+print('Amount of recipes:', len(recipes))'''
+
+# The tkinter GUI.
+root = tk.Tk()
+root.title('Italian-recipes')
+height=600
+width=700
+
+canvas = tk.Canvas(root, height=height, width=width)
+canvas.pack()
+
+background_image = tk.PhotoImage(file='food.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='#AFF9A9', bd=5)
+frame.place(relwidth=0.75, relheight=0.1, relx=0.5, rely=0.1, anchor='n')
+
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=5)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+button = tk.Button(frame, text='Enter food or ingredient', font=40)
+button.place(relx=0.55, relwidth=0.45, relheight=1)
+
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
+
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.5, relheight=1)
+
+root.mainloop()
